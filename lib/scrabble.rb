8e940d21c39bee2f00381da9deb1c0cd7ab4ded1
Hugh Morrison <hmorri32@gmail.com>
2017-08-30 09:22:31 -0600

    extract logic to helper functions, all functionality complete

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,45 @@
-class Scrabble
+require_relative 'dictionary'
 
-  def score(word)
-    1
+class Scrabble
+  include Dictionary 
+
+  attr_accessor :score
+  
+  def initialize 
+    @score = 0
   end
 
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
+  def score(word)
+    return @score = 0 if word.nil? || word.empty? 
+    reset_score
+    plus_ten_if_over_seven(word)
+    formatted = format(word)
+    formatted.each {|letter| @score += point_values[letter]}
+    @score 
+  end
+
+  def score_with_multipliers(word, array, multiplier = 1)
+    plus_ten_if_over_seven(word)
+    formatted = format(word)
+    formatted.each.with_index do |letter, index|
+      @score += point_values[letter] * array[index]            
+    end
+    @score * multiplier
+  end
+
+  def format(word)
+    word.upcase.chars 
+  end
+
+  def reset_score
+    @score = 0    
+  end
+
+  def seven?(word)
+    word.length >= 7
+  end
+
+  def plus_ten_if_over_seven(word)
+    @score += 10 if seven?(word)
   end
 end
